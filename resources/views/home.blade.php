23381f3104eda1fff31bea8b892b97198fe57874
Jamie Quinn <quinn-j55@ulster.ac.uk>
2023-06-28 02:43:09 +0100

    breeze auth added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,92 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <title>User Information</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 20px;
+    }
+
+    h1 {
+      text-align: center;
+    }
+
+    .container {
+      max-width: 400px;
+      margin: 0 auto;
+    }
+
+    .form-group {
+      margin-bottom: 20px;
+    }
+
+    .form-group label {
+      display: block;
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+
+    .form-group input,
+    .form-group select {
+      width: 100%;
+      padding: 10px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+    }
+
+    .form-group input[type="submit"] {
+      background-color: #4CAF50;
+      color: white;
+      cursor: pointer;
+    }
+
+    .form-group input[type="submit"]:hover {
+      background-color: #45a049;
+    }
+  </style>
 </head>
 <body>
-    welcome to my site
+  <div class="container">
+    <h1>User Information</h1>
+    <form>
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" placeholder="Enter your name" required>
+      </div>
+      <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" placeholder="Enter your email address" required>
+      </div>
+      <div class="form-group">
+        <label for="start-date">Start Date:</label>
+        <input type="date" id="start-date" name="start-date" required>
+      </div>
+      <div class="form-group">
+        <label for="phone">Phone Number:</label>
+        <input type="tel" id="phone" name="phone" placeholder="Enter your phone number" required>
+      </div>
+      <div class="form-group">
+        <label for="end-date">End Date:</label>
+        <input type="date" id="end-date" name="end-date" required>
+      </div>
+      <div class="form-group">
+        <label for="min-number">Number (minimum 5 digits):</label>
+        <input type="number" id="min-number" name="min-number" min="10000" required>
+      </div>
+      <div class="form-group">
+        <label for="max-number">Number (maximum 8 digits):</label>
+        <input type="number" id="max-number" name="max-number" max="99999999" required>
+      </div>
+      <div class="form-group">
+        <label for="whole-number">Whole Number (greater than 0):</label>
+        <input type="number" id="whole-number" name="whole-number" min="1" step="1" required>
+      </div>
+      <div class="form-group">
+        <input type="submit" value="Submit">
+      </div>
+    </form>
+  </div>
 </body>
 </html>
