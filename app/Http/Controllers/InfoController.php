c4d6aa97302a3a226e2cbd5afb2d2ecd639c9822
Jamie Quinn <quinn-j55@ulster.ac.uk>
2023-06-28 02:49:18 +0100

    info added

---
+++ app/Http/Controllers/InfoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\info;
+use App\Http\Requests\StoreinfoRequest;
+use App\Http\Requests\UpdateinfoRequest;
+
+class InfoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreinfoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(info $info)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(info $info)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateinfoRequest $request, info $info)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(info $info)
+    {
+        //
+    }
+}
